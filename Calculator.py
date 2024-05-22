5501e05ef085fd24a8abfd6140a4f53795d4229d
Miles F <miles.fritzmather@gmail.com>
2024-05-21 20:23:02 -0500

    Added inequalities and changed around how the set new variable command works. Now trying to make the absolute operation work and add parenthesis

--- Calculator.py
+++ Calculator.py
@@ -1,19 +1,10 @@
 from typing import Callable
 import os
 
-def stitch_in(str: str, start: int, end: int, stitch_in: str) -> str:
-    if start < 0: raise ValueError("start must be greater than or equal to 0.")
-    if end > len(str): raise ValueError("end must be less than or equal to length of template string.")
-    if start > end: raise ValueError("Start can not be larger than end.")
-
-    left_str = str[ :start]
-    right_str = str[end: ]
-    return left_str + stitch_in + right_str
-
 class operation:
     key: str
     importance: int
-    gather_values_func: Callable[..., list[float]]
+    gather_values_func: Callable[..., tuple[tuple[float, float] | str, tuple[float, float]]]
     math_func: Callable[..., float]
 
     def __init__(self, key, importance, gather_values_func, math_func):
@@ -21,20 +12,121 @@ class operation:
         self.importance = importance
         self.gather_values_func = gather_values_func
         self.math_func = math_func
+class Calculator:
+    commands: dict[str, Callable[..., str]]     # Probaly is static but is a keyword followed by the function to run with it
+    functions: dict[str, Callable[..., str]]    # A list of all functions
+    operations: list[operation]
+    variables: dict[str, str]                   # Dictionary of name for variable and the value to replace it with
+    past_equations: list[tuple[str, str]]       # A list of two strings: input & output
+    special_conditions: dict[str, any]          # Not sure if this is neccesary but holds any special conditions that dont fit anywhere else in a dictionary by name and value
+    round_to: int                               # -1 means no auto rounding
+
+    def __init__(self):
+        self.past_equations = [ ]
+        self.special_conditions = { }
+        self.variables = { 
+            "ans" : "0",
+            "pi" : "3.1415926",
+            "e" : "1.618"
+        }
+        self.commands = {
+            "var": (add_variable, True),
+            "help": (help, False),
+            "clear": (clear_console, False),
+            "round_to": (set_auto_round, True)
+        }
+        self.functions = {
+            "avg" : average,
+            "sum" : sum,
+            "mean" : mean
+        }
+        self.operations = [
+            operation("+", 1, two_around, lambda x, y: x + y),
+            operation("-", 1, two_around, lambda x, y: x - y),
+            operation("*", 2, two_around, lambda x, y: x * y),
+            operation("/", 2, two_around, lambda x, y: x / y),
+            operation("^", 2, two_around, lambda x, y: x ** y),
+            operation("%", 2, two_around, lambda x, y: x % y),
+            operation("|", 3, in_between, lambda x: abs(x))
+        ]
+
+    @staticmethod
+    def static_solve(equation: str) -> str:
+        operator_index, operation = find_next_operator(Calculator(), equation)
+        # print(f"operator index @ solve: {operator_index} | equation = {equation}")
+        if (operation == None):
+            return equation
+
+        math_func: Callable[..., float] = operation.math_func
+        values_func = operation.gather_values_func
+        values, bounds = values_func(equation, operator_index)
+
+        result = str(math_func(*values))
+        # print(f"before end: ({bounds[0]}, {bounds[1]}) was {equation[bounds[0]:bounds[1]]} but now is {result} | whole start: {equation} turned to {stitch_in(equation, bounds[0], bounds[1], result)}")
+        return Calculator.static_solve(stitch_in(equation, bounds[0], bounds[1], result))
+
+    #region Object Specific Functions
+    def solve(self, equation: str, clean: bool = False) -> str:
+        if clean:
+            equation = equation.replace(" ", "")
+            equation = replace_vars(self, equation)
+
+        operator_index, operation = find_next_operator(self, equation)
+        # print(f"operator index @ solve: {operator_index} | equation = {equation}")
+        if (operation == None):
+            return equation
+
+        math_func: Callable[..., float] = operation.math_func
+        values_func = operation.gather_values_func
+        values, bounds = values_func(equation, operator_index)
+        print("type: " + type(values)) 
+        if type(values) == "str":
+            result = str(math_func(self.solve(values)))
+        else:
+            result = str(math_func(*values))
+        # print(f"before end: ({bounds[0]}, {bounds[1]}) was {equation[bounds[0]:bounds[1]]} but now is {result} | whole start: {equation} turned to {stitch_in(equation, bounds[0], bounds[1], result)}")
+        return self.solve(stitch_in(equation, bounds[0], bounds[1], result))
+    def solve_f(self, equation: str) -> float | None:
+        try:
+            return float(self.solve(equation))
+        except ValueError as e:
+            return None
+
+    def run(self, input: str) -> str:
+        input = input.replace(" ", "")
+        if (cmd_output := was_command(self, input)) is not None:
+            return cmd_output
+        
+        input = replace_vars(self, input)
+        if (equations := is_inequality(self, input)) is not None:
+            # print(f"is inequality: {equations}")
+            return check_equality(self, *equations)
+
+        return self.solve(input)
+    #endregion
 
 #region COMMANDS
 def clear_console():
     os.system('cls')
     return ''
 
-def echo(*input: str):
-    return f"Echoing message: ({" | ".join(input)})"
-
 def help():
     return "This is the help function...not much here yet."
+
+def set_auto_round(self: Calculator, input: int):
+    self.round_to = input
+    return f"Now will automatically round to {self.round_to} decimal places. You change it to round all digits by typing 'sigfigs: True'"
+
+def add_variable(self: Calculator, input: str):
+    var_name, var_value = input.split("=")
+
+    if var_value is None: raise ValueError("Invalid syntax. Must have a '=' to assign a variable")
+
+    self.variables[var_name] = self.solve(var_value, clean=True)
+    return f"Added variable, '{var_name}' with a value of '{self.variables[var_name]}'"
 #endregion
 
-#region Math Functions
+#region parsing equation
 def is_number_char(input: str) -> bool:
     return input.isdigit() or input == "." or input == '-'
 
@@ -60,82 +152,28 @@ def two_around(equation: str, index: int) -> tuple[tuple[float, float], tuple[in
     return ((left_value, right_value), (start_index, end_index))
 
 def in_between(equation: str, start_index: int) -> float:
-    ...
+    for i in range(start_index+1, len(equation)):
+        if equation[i] == equation[start_index]:
+            return equation[start_index+1:i]
+    raise ValueError("Does not have enclosing wrapper.")
 
+
+
+#endregion
+
+#region Math Functions
 def average(*args: float):
     return sum(args)/len(args)
+
 def sum(*args: float):
     sum: float = 0
     for num in args: sum += num
     return sum
+
 def mean(*args: float):
     index: int = len(args) / 2
     return args[index]
 #endregion
-
-class Calculator:
-    commands: dict[str, Callable[..., str]]     # Probaly is static but is a keyword followed by the function to run with it
-    functions: dict[str, Callable[..., str]]    # A list of all functions
-    operations: list[operation]
-    variables: dict[str, str]                  # Dictionary of name for variable and the value to replace it with
-    past_equations: list[tuple[str, str]]       # A list of two strings: input & output
-    special_conditions: dict[str, any]          # Not sure if this is neccesary but holds any special conditions that dont fit anywhere else in a dictionary by name and value
-
-    def __init__(self):
-        self.past_equations = [ ]
-        self.special_conditions = { }
-        self.variables = { 
-            "ans" : "0",
-            "pi" : "3.1415926",
-            "e" : "1.618"
-        }
-        self.commands = {
-            "help": help,
-            "echo": echo,
-            "clear": clear_console
-        }
-        self.functions = {
-            "avg" : average,
-            "sum" : sum,
-            "mean" : mean
-        }
-        self.operations = [
-            operation("+", 1, two_around, lambda x, y: x + y),
-            operation("-", 1, two_around, lambda x, y: x - y),
-            operation("*", 2, two_around, lambda x, y: x * y),
-            operation("/", 2, two_around, lambda x, y: x / y),
-            operation("^", 2, two_around, lambda x, y: x ** y),
-            operation("%", 2, two_around, lambda x, y: x % y),
-            # "|": (in_between, lambda x: abs(x))
-        ]
-
-    #region Object Specific Functions
-    def solve(self, equation: str) -> str:
-        operator_index, operation = find_next_operator(self, equation)
-        # print(f"operator index @ solve: {operator_index} | equation = {equation}")
-        if (operation == None):
-            return equation
-
-        math_func: Callable[..., float] = operation.math_func
-        values_func: Callable[..., list[float]] = operation.gather_values_func
-        values, bounds = values_func(equation, operator_index)
-
-        result = str(math_func(*values))
-        # print(f"before end: ({bounds[0]}, {bounds[1]}) was {equation[bounds[0]:bounds[1]]} but now is {result} | whole start: {equation} turned to {stitch_in(equation, bounds[0], bounds[1], result)}")
-        return self.solve(stitch_in(equation, bounds[0], bounds[1], result))
-    
-    def run(self, input: str) -> str:
-        input = input.replace(" ", "")
-        if (cmd_output := was_command(self, input)) is not None:
-            return cmd_output
-        
-        input = replace_vars(self, input)
-        # print(f"formatted equation: {input}")
-        return self.solve(input)
-
-    def add_var(self, new_var: tuple[str, str]) -> None:
-        self.variables[new_var[0]] = new_var[1]
-    #endregion
 
 #region HELPERS
 def starts_with_var(self: Calculator, substr: str) -> str | None:
@@ -154,34 +192,96 @@ def replace_vars(self: Calculator, input: str) -> str:
         i += 1
     return input
 
-def was_command(self: Calculator, input: str) -> str | None:
-    if len((vars := tuple(input.split(":")))) > 1:
-        self.add_var(vars)
-        return f"Added variable '{vars[0]}' with value '{vars[1]}'"
-    else:
-        if (param_start := input.find("[")) != -1:
-            parameters: list[str] = input[param_start + 1 : input.find("]")].split(",") # list of all values split by a comma within the brackets
-            input = input[ :param_start] #Set input to be just the command
-        else:
-            parameters = None
-
-        for cmd in self.commands:
-            if input == cmd:
-                if parameters is not None:
-                    return self.commands[cmd](parameters)
-                else:
-                    return self.commands[cmd]()
-            
-    return None
-
 def find_next_operator(self: Calculator, equation: str) -> tuple[int, operation]:
+    print(f"NEW NEW NEW Input: '{equation}'")
     current_operation = (-1, None)
-    for i in range(0 if equation[0] != '-' else 1, len(equation)):
+    start_index = 0 if equation[0] != '-' else 1
+    for i in range(start_index, len(equation)):
+        print(f"i: {i} | len: {len(equation)}")
         for operator in self.operations:
-            if equation[i:i+len(operator.key)] == operator.key:
+            if i+len(operator.key) < len(equation) and equation[i:i+len(operator.key)] == operator.key:
                 if (current_operation[1] is None or operator.importance > current_operation[1].importance):
                     current_operation = (i, operator)
                 break
-    
+
     return current_operation
+
+def stitch_in(str: str, start: int, end: int, stitch_in: str) -> str:
+    if start < 0: raise ValueError("start must be greater than or equal to 0.")
+    if end > len(str): raise ValueError("end must be less than or equal to length of template string.")
+    if start > end: raise ValueError("Start can not be larger than end.")
+
+    left_str = str[ :start]
+    right_str = str[end: ]
+    return left_str + stitch_in + right_str
 #endregion
+
+def was_command(self: Calculator, input: str) -> str | None:
+    if (param_start := input.find(":")) != -1:
+        parameters: list[str] = input[param_start + 1: ].split(",") # list of all values split by a comma within the brackets
+        input = input[ :param_start] #Set input to be just the command
+    else:
+        parameters = None
+
+    for cmd in self.commands:
+        if input == cmd:
+            func, requires_calc_reference = self.commands[cmd]
+            if requires_calc_reference: parameters.insert(0, self)
+    
+            if parameters is not None:
+                return func(*parameters)
+            else:
+                return func()
+        
+    return None
+
+def is_inequality(self: Calculator, input: str) -> tuple[str, str, str] | None:
+    equal = input.find("=")
+    greater = input.find(">")
+    less = input.find("<")
+
+    if equal != -1:
+        if greater == equal - 1:
+            return (input[ :greater], input[equal+1: ], "greater or equal")
+        elif less == equal - 1:
+            return (input[ :less], input[equal+1: ], "less or equal")
+        elif input[equal+1] == '=':
+            return (input[ :equal], input[equal+2: ], "equal")
+        else:
+            raise ValueError("Should not be possible. Misstyped")
+    elif greater != -1:
+        return (input[ :greater], input[greater+1: ], "greater")
+    elif greater != -1:
+        return (input[ :less], input[less+1: ], "less")
+
+    return None
+
+def check_equality(self: Calculator, left_equation: str, right_equation: str, equality_check: str) -> str:
+    match equality_check:
+        case "equal":
+            if (left_solve := self.solve_f(left_equation)) == (right_solve := self.solve_f(right_equation)):
+                return f"Correct. Results: (left: {left_solve}, right: {right_solve})"
+            else:
+                return f"Incorrect. Results: (left: {left_solve}, right: {right_solve})"
+        case "greater":
+            if (left_solve := self.solve_f(left_equation)) > (right_solve := self.solve_f(right_equation)):
+                return f"Correct. Results: (left: {left_solve}, right: {right_solve})"
+            else:
+                return f"Incorrect. Results: (left: {left_solve}, right: {right_solve})"
+        case "less":
+            if (left_solve := self.solve_f(left_equation)) < (right_solve := self.solve_f(right_equation)):
+                return f"Correct. Results: (left: {left_solve}, right: {right_solve})"
+            else:
+                return f"Incorrect. Results: (left: {left_solve}, right: {right_solve})"
+        case "greater or equal":
+            if (left_solve := self.solve_f(left_equation)) >= (right_solve := self.solve_f(right_equation)):
+                return f"Correct. Results: (left: {left_solve}, right: {right_solve})"
+            else:
+                return f"Incorrect. Results: (left: {left_solve}, right: {right_solve})"
+        case "less or equal":
+            if (left_solve := self.solve_f(left_equation)) <= (right_solve := self.solve_f(right_equation)):
+                return f"Correct. Results: (left: {left_solve}, right: {right_solve})"
+            else:
+                return f"Incorrect. Results: (left: {left_solve}, right: {right_solve})"
+        case _:
+            raise ValueError("Invalid equality check value. Please have it be greater, less, equal or some combination.")
--- run.py
+++ run.py
@@ -20,6 +20,12 @@ def run_program():
             print(f"Result: '{result}'")
 
 def run_tests():
+    setup_commands = [
+        "x:100",
+        "y:9",
+        "z:33"
+    ]
+
     tests = [
         ("112+9", "121.0"),
         ("100+-10", "90.0"),
@@ -29,7 +35,15 @@ def run_tests():
         ("3%4", "3.0"),
         ("4%3", "1.0"),
         ("4%3*10-6--9/3", "7.0"),
+        ("12+2==14", "Correct"),
+        ("12-2==9", "Incorrect"),
+        ("12*2--6/3==12*2--6/3", "Correct"),
+        ("x==z", "Incorrect"),
+        ("x^2", "10000.0")
     ]
+
+    for command in setup_commands:
+        calculator.run(command)
 
     for num in range(len(tests)):
         test = tests[num]
